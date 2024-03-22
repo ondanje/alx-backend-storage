dfc8a2873108c1d501a90a109c757fc36e82efd8
ondanje <cyrilondanje@gmail.com>
2024-03-22 11:36:53 +0300

    creating unique user

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -4,4 +4,7 @@
 -- email, string (255 characters), never null and unique
 -- name, string (255 characters)
 
-CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT UNIQUE PRIMARY KEY NOT NULL, email VARCHAR(255) NOT NULL, name VARCHAR(255));
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT UNIQUE PRIMARY KEY NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	name VARCHAR(255));
