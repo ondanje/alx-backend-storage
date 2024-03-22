cb18269048559a1547858db3b762aa29e77def96
ondanje <cyrilondanje@gmail.com>
2024-03-22 11:33:09 +0300

    creating unique user

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- creates a table users following these requirements:
+-- With these attributes:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+
+CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT UNIQUE PRIMARY KEY NOT NULL, email VARCHAR(255) NOT NULL, name VARCHAR(255));
