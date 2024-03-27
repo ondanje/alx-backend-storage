d4484be0550225c83aeb09c10dba89dd0e7b8626
ondanje <cyrilondanje@gmail.com>
2024-03-27 23:10:05 +0300

    tasks

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,16 +2,65 @@
 """
 importing redis, typing and uuid modules
 """
-from types import UnionType
+from functools import wraps
+import sys
 import redis
-import typing
 from typing import Union, Optional, Callable
 import uuid
-import sys
+
+
+Union_Types = Union[str, bytes, int, float]
+
+
+def replay(method: Callable) -> None:
+    """
+    display the history of calls of afunction
+    """
+    key = method.__qualname__
+    data = redis.Redis()
+    hist = data.get(key).decode("utf-8")
+    print("{} was called {} times:".format(key, hist))
+    inputs = data.lrange(key + ":inputs", 0, -1)
+    outputs = data.lrange(key + ":outputs", 0, -1)
+    for k, v in zip(inputs, outputs):
+        print(f"{key}(*{k.decode('utf-8')}) -> {v.decode('utf-8')}")
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Increments a counter in Redis every time
+    the function is called.
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function"""
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    Records the history of input arguments and output of
+    the method
+    """
+    key = method.__qualname__
+    i = "{}{}".format(key, ":inputs")
+    o = "{}{}".format(key, ":outputs")
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.rpush(i, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(o, str(res))
+        return res
+    return wrapper
 
 
 class Cache:
-    def __init__(self) -> None:
+    def __init__(self):
         """
         __init__ method, store an instance of the Redis
         client as a private variable
@@ -19,38 +68,37 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
+    @count_calls
+    @call_history
+    def store(self, data: Union_Types) -> str:
         """
         method that takes a data argument and returns a string
         the data can be a str, bytes, int or float
         """
         key = str(uuid.uuid4())
-        self._redis.set(key, data)
-
+        self._redis.mset({key: data})
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> UnionType:
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union_Types:
         """
         get method that take a key string argument and
         an optional Callable argument named fn
         """
-        result = self._redis.get(key)
-        if result is None:
-            return None
-        if fn is not None:
-            result = fn(result)
-        return result
+        if fn:
+            return fn(self._redis.get(key))
+        data = self._redis.get(key)
+        return data
 
-    def get_str(self, key: str) -> typing.Union[str, None]:
+    def get_int(self, key: str) -> int:
         """
         get_str and get_int that will automatically parametrize
         Cache.get with the correct conversion function.
         """
-        return self.get(key, lambda x: x.decode('utf-8'))
+        return int.from_bytes(self.get(key), sys.byteorder)
 
-    def get_int(self, key: str) -> typing.Union[int, None]:
+    def get_str(self, key: str) -> str:
         """
         get_str and get_int that will automatically parametrize
         Cache.get with the correct conversion function.
         """
-        return self.get(key, int)
+        return self.get(key).decode("utf-8")
